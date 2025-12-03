d2dafb9ebedd324dda6b25c0481eef36bc74f744
Mohammad Niyas <mohammadniyas662@gmail.com>
2025-12-03 09:20:23 +0530

    logout controller changed

--- controllers/userLogout.go
+++ controllers/userLogout.go
@@ -7,5 +7,5 @@ import (
 )
 
 func UserLogoutHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"message": "Logout successfull"})
+	c.JSON(http.StatusOK, gin.H{"message": "Logout successfully"})
 }
